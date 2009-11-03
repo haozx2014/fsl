d7b1ed412c231dc4544f43993a4aa1c8bc8d1384
Michael Hanke <michael.hanke@gmail.com>
2009-11-03 16:01:11 -0500

    Adjust config and docs for SGE setup.

--- debian/fsl.sh
+++ debian/fsl.sh
@@ -9,15 +9,12 @@
 
 # Debian has a fixed FSLDIR
 FSLDIR=/usr/share/fsl
-export FSLDIR
 
 # Possum is installed in the same directory
 POSSUMDIR=$FSLDIR
-export POSSUMDIR
 
 # add the fsl binary path to the search path
 PATH=$PATH:/usr/lib/fsl
-export PATH
 
 # The following variable selects the default output image type
 # Legal values are:
@@ -25,37 +22,45 @@ export PATH
 # This would typically be overwritten in ${HOME}/.fsl/fsl.sh if the user
 # wished to write files with a different format
 FSLOUTPUTTYPE=NIFTI_GZ
-export FSLOUTPUTTYPE
 
 # Comment out the definition of FSLMULTIFILEQUIT to enable
 #  FSL programs to soldier on after detecting multiple image
 #  files with the same basename ( e.g. epi.hdr and epi.nii )
-FSLMULTIFILEQUIT=TRUE ; export FSLMULTIFILEQUIT
+FSLMULTIFILEQUIT=TRUE
 
 # The following variables specify paths for programs and can be changed
-#  or replaced by different programs ( e.g. FSLDISPLAY=open   for MacOSX)
-
+# or replaced by different programs, by default set sensible Debian-defaults
 FSLTCLSH=/usr/bin/tclsh
 FSLWISH=/usr/bin/wish
-
 FSLBROWSER=/etc/alternatives/x-www-browser
 
-export FSLTCLSH FSLWISH FSLBROWSER
 
 
 # The following variables are used for running code in parallel across
 #  several machines ( i.e. for FDT )
-
+# for a cluster engine setup see below
 FSLLOCKDIR=
 FSLMACHINELIST=
 FSLREMOTECALL=
 
-export FSLLOCKDIR FSLMACHINELIST FSLREMOTECALL
+# If set, tell FSL to use Sun Gridengine to submit jobs instead of running them
+# directly on the machine. If unset, no attempt will be made to utilize
+# gridengine, even if it is running.
+#FSLPARALLEL=1
+FSLPARALLEL=
+# Mail setup for gridengine jobs. See man qsub (-m option) for all possible
+# settings. By default no email is sent.
+FSLCLUSTER_MAILOPTS="n"
+
 
 
 ###################################################
 ####    DO NOT ADD ANYTHING BELOW THIS LINE    ####
 ###################################################
+
+export FSLDIR POSSUMDIR PATH FSLMULTIFILEQUIT FSLOUTPUTTYPE FSLTCLSH \
+       FSLWISH FSLBROWSER FSLLOCKDIR FSLMACHINELIST FSLREMOTECALL
+
 
 # Configure the linker search path for Debian FSLs internal shared libraries
 LD_LIBRARY_PATH=/usr/lib/fsl${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
